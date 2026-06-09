4ee7bb197ca9533083053bb0702a388497bcf30b
HaMeNopeDiot <staruhind@yandex.ru>
2026-06-09 15:41:05 +0300

    update rtl.f

--- hardware/rtl.f
+++ hardware/rtl.f
@@ -10,6 +10,9 @@ rtl/thread_unit.sv
 rtl/interfaces/reg_if.sv
 rtl/interfaces/simple_bus_if.sv
 rtl/interfaces/simple_hndh_if.sv
+rtl/interfaces/ahb4_bus_if.sv
+rtl/utils/ahb_pkg.sv
+rtl/utils/ahb_master.sv
 rtl/core_decoder.sv
 rtl/core_lsu_fsm_pkg.sv
 rtl/core_lsu.sv
