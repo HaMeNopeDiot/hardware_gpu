9ead50c428affa4f8206e6b8852d9c752fcc0694
HaMeNopeDiot <staruhind@yandex.ru>
2026-06-26 17:03:34 +0300

    test upddate

--- hardware/tb/core/tests/vkcube_core_test.py
+++ hardware/tb/core/tests/vkcube_core_test.py
@@ -20,48 +20,14 @@ from core.core_enums        import RoundModeE
 
 from fpu.fppconverter     import ieee754_to_float
 
+from utility.bin_unpack   import unpack_bin_file, read_vbuffer
 from utility.addresess    import CSRAddr
 
-
-def read_vbuffer(filename):
-    values = []
-    try:
-        with open(filename, 'r', encoding='utf-8') as f:
-            for line in f:
-                line = line.strip()
-
-                # Пропускаем пустые строки и комментарии
-                if not line or line.startswith('//'):
-                    continue
-
-                # Разбиваем строку на части по пробелам
-                parts = line.split()
-
-                for part in parts:
-                    # На всякий случай проверяем, не является ли часть комментарием
-                    if part.startswith('//'):
-                        break
-
-                    if part is not None:
-                        values.append(part)
-    except FileNotFoundError:
-        print(f"Файл {filename} не найден.")
-        return []
-
-    return values
 
 class VKCubeTest(BaseCoreTest):
     def prepare(self):
         cocotb.log.info(f"It's comming")
-        with open("../../nir_to_assembly/vertex_shader.bin", "rb") as f:
-            raw_data = f.read()
-
-        # Отрезаем лишние байты, если длина не кратна 4
-        count = len(raw_data) // 4
-        raw_data = raw_data[:count * 4]
-
-        # '<I' = little-endian, unsigned int
-        words = struct.unpack('<' + 'I' * count, raw_data)
+        words = unpack_bin_file("../../nir_to_assembly/vertex_shader.bin")
         idx = 0
 
         i_arr = []
