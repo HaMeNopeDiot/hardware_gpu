7d68be8c31440799f2530f2d83df959f9601f2ba
HaMeNopeDiot <staruhind@yandex.ru>
2026-07-01 14:12:06 +0300

    Fix AHB slave

--- hardware/rtl/utils/ahb_mem.sv
+++ hardware/rtl/utils/ahb_mem.sv
@@ -8,27 +8,32 @@
 
 
 module ahb_mem #(
-    parameter int DEPTH = 1024,
-    parameter int AW = 32,
-    parameter int DW = 32
+    parameter int DEPTH             = 1024,
+    parameter int AW                = 32,
+    parameter int DW                = 32,
+    parameter int STRB              = DW / 8,
+    parameter int UNALIGNED_BITS    = $clog2(STRB),
+    parameter bit DEBUG_MODE        = 0,
+    parameter int DEBUG_REGS_TRACE  = 10,
+    parameter int DEBUG_TRACE_OFS   = 0
 )(
     input  logic                HCLK,
     input  logic                HRESETn,
-    input  logic [AW-1:0]       HADDR,
-    input  logic [1:0]          HTRANS,
-    input  logic [DW-1:0]       HWDATA,
+    input  logic [AW    - 1: 0] HADDR,
+    input  logic [1        : 0] HTRANS,
+    input  logic [DW    - 1: 0] HWDATA,
     input  logic                HWRITE,
 
-    output logic [DW-1:0]       HRDATA
+    output logic [DW    - 1: 0] HRDATA
 );
 
-logic [DW-1:0] mem[DEPTH];
+logic [DW - 1: 0] mem[DEPTH];
 
 always_ff @(posedge HCLK) begin: always_read_blk
-    HRDATA <= mem[HADDR];
+    HRDATA <= mem[HADDR >> UNALIGNED_BITS];
 end
 
-logic [AW-1:0] address;
+logic [AW - 1: 0] address;
 logic          do_write;
 always_ff @(posedge HCLK or negedge HRESETn) begin
     if (!HRESETn) begin
@@ -36,7 +41,7 @@ always_ff @(posedge HCLK or negedge HRESETn) begin
         do_write <= '0;
     end
     else begin
-        address <= HADDR;
+        address  <= HADDR >> UNALIGNED_BITS;
         do_write <= (HWRITE == '1) && (HTRANS != '0);
     end
 end
@@ -44,6 +49,15 @@ end
 always_ff @(posedge HCLK) begin
     if (do_write) begin
         mem[address] <= HWDATA;
+    end
+end
+
+if (DEBUG_MODE) begin: gen_debug_reg_trace
+    logic [DW - 1: 0] trace_regs [DEBUG_REGS_TRACE];
+    for (genvar i = 0; i < DEBUG_REGS_TRACE; i++) begin: gen_trace_reg
+        logic [DW - 1: 0] trace_reg;
+        assign trace_reg        = mem[i + DEBUG_TRACE_OFS];
+        assign trace_regs[i]    = trace_reg;
     end
 end
 
